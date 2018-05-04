85029701773158f3a1b9f01cd71e00d502d6ff42
seshadrir <seshadri.iitd@gmail.com>
2018-05-04 16:18:29 +0530

    Update 1062-containers.cpp

--- cp/1062-containers.cpp
+++ cp/1062-containers.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
 #include <vector>
+#include <iostream>
 #include <algorithm>
 #include <iterator>
 #include <cassert>
 #include <queue>
 #include <bitset>
 #include <set>
+#include <stack>
 #include <map>
 #include <string>
-#include <stack>
 
 using namespace std;
 
@@ -21,58 +21,24 @@ int main()
         if(s == end)
             return 0;
         vector<stack<char>> cont;
-        vector<int> count(26, 0);
-        for(unsigned int i = 0; i < s.size(); ++i)
-            count[s[i] - 'A']++;
 
-        for(int i = 0; i < s.size(); ++i){
-            const unsigned char c = s[i];
-            int index = -1;
-            for(unsigned int j = 0; j < cont.size(); ++j){
-                if(cont[j].top() == c){
-                    index = j;
-                    cont[j].push(c);
+        for(size_t j = 0; j < s.size(); ++j){
+            unsigned int i = 0;
+            for(; i < cont.size(); ++i){
+                if(s[j] <= cont.at(i).top()  ){
+                    cont.at(i).push(s[j]);
                     break;
                 }
             }
-
-            if(index == -1){
-                for(unsigned int j = 0; j < cont.size(); ++j){
-                    if(cont[j].empty())
-                        continue;
-                    char tc = cont[j].top();
-                    if((count[tc-'A'] == 0) && (c < tc)){
-                        cont[j].push(c);
-                        index = j;
-                        break;
-                    }
-                }
+            if(i == cont.size()){
+                stack<char> ss;
+                ss.push(s[j]);
+                cont.push_back(ss);
             }
-            if(index == -1){
-                for(unsigned int j = 0; j < cont.size(); ++j){
-                    if(cont[j].size() == 0){
-                        cont[j].push(c);
-                        index = j;
-                    }
-                }
-            }
-            if(index == -1){
-                stack<char> sss;
-                sss.push(c);
-                cont.push_back(sss);
-            }
-
-            count[c-'A']--;
-
         }
 
 
-        unsigned int a = 0;
-        for(unsigned int i = 0; i < cont.size(); ++i){
-            a = (cont[i].size() == 0) ? a : a+1;
-        }
-
-        cout << "Case " << c << ": " << a << endl;
+        cout << "Case " << c << ": " << cont.size() << endl;
         c++;
     }
 }
