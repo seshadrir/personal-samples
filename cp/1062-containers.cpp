fa973ec93c57691fa366b9132a7dfb048e3bef54
seshadrir <seshadri.iitd@gmail.com>
2018-07-28 10:59:14 +0530

    check in message

--- cp/1062-containers.cpp
+++ cp/1062-containers.cpp
@@ -21,58 +21,42 @@ int main()
         if(s == end)
             return 0;
         vector<stack<char>> cont;
-        vector<int> count(26, 0);
+        vector<int> count(26, 0), stackLoc(26, -1), inStack(26, 0);
+
         for(unsigned int i = 0; i < s.size(); ++i)
             count[s[i] - 'A']++;
 
         for(int i = 0; i < s.size(); ++i){
-            const unsigned char c = s[i];
-            int index = -1;
-            for(unsigned int j = 0; j < cont.size(); ++j){
-                if(cont[j].top() == c){
-                    index = j;
-                    cont[j].push(c);
-                    break;
-                }
-            }
 
-            if(index == -1){
-                for(unsigned int j = 0; j < cont.size(); ++j){
-                    if(cont[j].empty())
-                        continue;
-                    char tc = cont[j].top();
-                    if((count[tc-'A'] == 0) && (c < tc)){
+            const unsigned char c = s[i];
+            unsigned int index = c - 'A';
+            if(stackLoc[index] != -1){
+                cont[stackLoc[index]].push(c);
+                inStack[index]++;
+            } else {
+                for(unsigned int  j = 0; j < cont.size(); ++j){
+                    const char ts = cont.at(j).top();
+                    unsigned int tsi = ts-'A';
+                    if(inStack[tsi] == count[tsi] && inStack[index] == 0 && c < ts ){
                         cont[j].push(c);
-                        index = j;
+                        stackLoc[index] = j;
+                        inStack[index]++;
                         break;
                     }
                 }
-            }
-            if(index == -1){
-                for(unsigned int j = 0; j < cont.size(); ++j){
-                    if(cont[j].size() == 0){
-                        cont[j].push(c);
-                        index = j;
-                    }
+                if(stackLoc[index] == -1){
+                    stack<char> sss;
+                    sss.push(c);
+                    inStack[index]++;
+                    stackLoc[index] = cont.size();
+                    cont.push_back(sss);
+
                 }
             }
-            if(index == -1){
-                stack<char> sss;
-                sss.push(c);
-                cont.push_back(sss);
-            }
-
-            count[c-'A']--;
-
         }
 
 
-        unsigned int a = 0;
-        for(unsigned int i = 0; i < cont.size(); ++i){
-            a = (cont[i].size() == 0) ? a : a+1;
-        }
-
-        cout << "Case " << c << ": " << a << endl;
+        cout << "Case " << c << ": " << cont.size() << endl;
         c++;
     }
 }
